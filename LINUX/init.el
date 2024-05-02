17a6ab8904b64f508ab44b0f2ee33e2b7791bddf
Lebohang <mmokhwibitjane023b@student.wethinkcode.co.za>
2024-05-02 13:19:50 +0200

    fixed trailing backslash problem

--- LINUX/init.el
+++ LINUX/init.el
@@ -4,8 +4,6 @@
 
 (add-to-list 'load-path "~/Dev/emacs/EmacsInit/Packages")
 
-(require )
-
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 (add-hook 'minibuffer-setup-hook 'my-minibuffer-setup)
@@ -38,23 +36,19 @@
 (set-face-attribute 'default nil :font "Cascadia Mono" :height 100)
 
 
-;; ###############  KEYBINDINGS  #####################
+;; ;; ###############  KEYBINDINGS  #####################
 
 ;; Make ECS quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-S-c") 'eshell)
 (global-set-key (kbd "C-x C-g") 'goto-line)
 (global-set-key (kbd "C-/") 'comment-or-uncomment-region)
-
-;; (require 'packages)
 
 ; Display line numer and column number 
 (display-line-numbers-mode)
 (column-number-mode)
 
-; Remove line numbers in shell mode
+;; ; Remove line numbers in shell mode
 (dolist (mode '(org-mode-hook
 		term-mode-hook
 		shell-mode-hook
@@ -72,113 +66,89 @@
   (package-refresh-contents))
 
 
+;; ; ############################ GET PACKAGES ##############################
 
-; ############################ GET PACKAGES ##############################
+;; ; install use-package
+;; (unless (package-installed-p 'use-package)
+;; 	(package-install 'use-package))
 
-; install counsel
-(unless (package-installed-p 'counsel)
-  (package-install 'counsel))
+;; (require 'use-package)
+;; (setq use-package-always-ensure t)
 
-; install use-package
-(unless (package-installed-p 'use-package)
-	(package-install 'use-package))
-
-(require 'use-package)
-(setq use-package-always-ensure t)
-
-(use-package command-log-mode)
+;; (use-package command-log-mode)
 
 ; install ivy
-(use-package ivy
-	     :diminish
-	     :bind(("C-s" . swiper)
-	     :map ivy-minibuffer-map
-	     ("TAB" . ivy-alt-done)
-	     ("C-l" . ivy-alt-done)
-	     ("C-j" . ivy-next-line)
-	     ("C-k" . ivy-previous-line)
-	     :map ivy-switch-buffer-map
-	     ("C-k" . ivy-previous-line)
-	     ("C-l" . ivy-done)
-	     ("C-d" . ivy-switch-kill-buffer)
-	     :map ivy-reverse-i-search-map
-	     ("C-k" . ivy-previous-line)
-	     ("C-d" . ivy-reverse-i-serach-kill))
-	     :config
-	     (ivy-mode 1)
-)
+(unless (package-installed-p 'ivy)
+  (package-install 'ivy))
+(ivy-mode 1)
+
+
+;; ; install counsel
+(unless (package-installed-p 'counsel)
+  (package-install 'counsel))
+(when (member 'ivy package-activated-list)
+  (counsel-mode 1))
 
 (unless (package-installed-p 'all-the-icons)
   (setq  new-all-icons-new-loadup t))
 
 
-(use-package all-the-icons
-  :ensure t
-  :if (display-graphic-p))
+(unless (package-installed-p 'all-the-icons)
+  (package-install 'all-the-icons))
 
 (unless (equal new-all-icons-new-loadup nil)
   (all-the-icons-install-fonts)
-  ;(nerd-icons-install-fonts)
+  (nerd-icons-install-fonts)
 )
 
-(use-package all-the-icons-dired
-  :hook (dired-mode . (lambda () (all-the-icons-dired-mode t)
-	)
-    )
-  )
+;; (use-package all-the-icons-dired
+;;   :hook (dired-mode . (lambda () (all-the-icons-dired-mode t)
+;; 	)
+;;     )
+;;   )
+
+(unless (package-installed-p 'all-the-icons-dired)
+  (package-install 'all-the-icons-dired))
+(add-hook 'dired-mode-hook '(lambda () (all-the-icons-dired-mode t)))
 
 (setq org-support-shift-select t)
 
+
 (makunbound  'new-all-icons-new-loadup)
 
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1))
 
+(unless (package-installed-p 'doom-modeline)
+  (package-install 'doom-modeline))
+(doom-modeline-mode 1)
 (setq doom-modeline-height 35)
 
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
+;(use-package rainbow-mode
+;  :hook org-mode prog-mode)
+;(rainbow-mode)
 
-(use-package rainbow-mode
-  :hook org-mode prog-mode)
-(rainbow-mode)
+(unless (package-installed-p 'rainbow-mode)
+  (package-install 'rainbow-mode))
+(rainbow-mode 1)
 
-;; (unless (package-installed-p 'ivy-rich)
-;;   (package-install 'ivy-rich))
-;; (ivy-rich-mode 1)
 
-(use-package glsl-mode)
+(unless (package-installed-p 'ivy-rich)
+  (package-install 'ivy-rich))
+(ivy-rich-mode 1)
+
+
+(unless (package-installed-p 'glsl-mode)
+  (package-install 'glsl-mode))
 (autoload 'glsl-mode "glsl-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.glsl\\'" . glsl-mode))
 
+
 ;; Projectile
-(use-package projectile
-  :config
-  (projectile-mode 1))
+(unless (package-installed-p 'projectile)
+  (package-install 'projectile))
+(projectile-mode 1)
+
 
 ;; Dashboard
-;; (use-package dashboard
-;;   :ensure t
-;;   :init
-;;   (setq initial-buffer-choice 'dashboard-open)
-;;   (setq dahsboard-set-heading-icons t)
-;;   (setq dashboard-set-file-icons t)
-;;   (setq dashboard-banner-logo-title "More Than a Text Editor")
-;;   (setq dashboard-starup-banner 'logo)
-;;   ;;(setq dashboard-startup-banner "path to pickture") ;; use custon banner
-;;   (setq dashboard-center-content nil)
-;;   (setq dashbboard-items '(
-;;     (recents . 5)
-;;     (agenda . 5)
-;;     (bookmarks . 3)
-;;     (projects . 5)))
-;;   (dashboard-modify-heading-icons '(
-;;     (recents . "file-text")
-;;     (bookkmarks . "book")))
-;;   :config
-;;   (dashboard-setup-startup-hook))
-
 (unless (package-installed-p 'dashboard)
   (package-install 'dashboard))
 
@@ -214,15 +184,21 @@
 (use-package company-box
   :after company
   :hook (company-mode . company-box-mode))
+(company-mode 1)
 
-
-; ESHELL 
+;; ; ESHELL 
 (setq eshell-rc-script (concat user-emacs-directory "eshell/profile")
       eshell-aliases-file (concat user-emacs-directory "eshell/aliases")
       eshell-scoll-to-bottom-on-input t
       eshell-destroy-buffer-when-process-dies t)
 
-(use-package markdown-mode
-  :ensure t
-  :mode ("FEADME\\.md\\" . gfm-mode)
-  :init (setq markdown-command "multimarkdown"))
+;; (use-package markdown-mode
+;;   :ensure t
+;;   :mode ("README\\.md\\" . gfm-mode)
+;;   :init (setq markdown-command "multimarkdown"))
+
+;; ###################################### PACKAGE KEYBINDINGS ########################################
+
+(global-set-key (kbd "C-x C-f") 'find-file)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-s") 'swiper)
